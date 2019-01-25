9229263599ab9c5206bdabd89c6aabfe0ff3b006
MarryYou <614345508@qq.com>
2019-01-25 18:18:37 +0800

    vh vw 2 rem

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./page/app";
 import Main from "./page/home";
 import Rank from "./page/rank";
+import "lib-flexible";
 import * as serviceWorker from "./serviceWorker";
 import {
   BrowserRouter as Router,
